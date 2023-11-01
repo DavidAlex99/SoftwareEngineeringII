3adea84cc39fa184e75e3f52e54f91707257b9d6
DavidAlex99 <70306437+DavidAlex99@users.noreply.github.com>
2023-11-01 14:29:31 -0500

    Create dinning.java

---
+++ dinning.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class dinning {
+	public static void main(String args[]) {
+		menu();
+	}
+	
+	public static void menu() {
+		System.out.println("Seleccione del menu: ");
+		System.out.println("1.pizza");
+		System.out.println("2.pollo");
+		System.out.println("3.lonchys");
+		System.out.println("4.grillete");
+		Scanner ob = new Scanner(System.in);
+		int op = ob.nextInt();
+		switch(op) {
+			case 1:
+				
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+		}
+	}
+}
